8e116d1f6a447e62fc5c12a47071a8e6c7f926b4
Paulo <pauloh.andrade@outlook.com.br>
2023-08-29 22:19:46 -0300

    feat: added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import Content from './components/Content'
 import Navbar from './components/Navbar'
 
+import reactLogo from './assets/react.svg'
+import Footer from './components/Footer';
+
+
 const App = () => {
+  let reactLogoTextoAlt = "Logo do React";
+
   return (
     <>
-      <Navbar></Navbar>
+      <Navbar/>
+      <Content logo={reactLogo} alt={reactLogoTextoAlt}/>
+      <Footer />
     </>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+            <ul>
+                <li><a href="#">GitHub</a></li>
+                <li><a href="#">X</a></li>
+                <li><a href="#">Reddit</a></li>
+            </ul>
+            </div>
+        </footer>
+    )
+}
+
+
+export default Footer
