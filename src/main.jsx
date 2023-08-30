4828f99c465d5e1e0adc1ab69c146e442ef70875
Paulo <pauloh.andrade@outlook.com.br>
2023-08-29 22:55:45 -0300

    feat: added router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
+
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+
 import './index.css'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import Home from './pages/Home.jsx'
+import Product from './pages/Product.jsx'
+import NotFound from './pages/NotFound.jsx'
+import UpdateProduct from './pages/UpdateProduct.jsx'
+
+const router = createBrowserRouter([{
+  path: "/",
+  element: <App />,
+  errorElement: <NotFound />,
+  children: [
+    { path: "/", element: <Home /> },
+    { path: "/produtos", element: <Product /> },
+    { path: "/produtos/editar/:id", element: <UpdateProduct /> }
+  ]
+}])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}> </RouterProvider>
   </React.StrictMode>,
 )
